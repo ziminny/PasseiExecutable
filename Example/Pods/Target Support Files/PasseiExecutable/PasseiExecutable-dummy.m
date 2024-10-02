3b71dbdcc2fd3a9ad51657aad30ee9d868407de2
Vagner Reis <ziminny@gmail.com>
2024-10-01 23:13:31 -0300

    initial commit

---
+++ Example/Pods/Target Support Files/PasseiExecutable/PasseiExecutable-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PasseiExecutable : NSObject
+@end
+@implementation PodsDummy_PasseiExecutable
+@end
---
+++ PasseiExecutable/Classes/ReplaceMe.swift
@@ -0,0 +1,3 @@
+public class ReplaceMe {
+    public init() {}
+}
